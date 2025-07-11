dc54a2c9f5cabe5c91cd2d95321f03ccd5db001c
rtmagalhaesOS <ricardo.magalhaes@outsystems.com>
2025-07-11 17:15:00 +0100

    Add project files.

---
+++ IRemoveDiacritics.cs
@@ -0,0 +1,12 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace OutSystems.RemoveDiacritics {
+
+    /// <summary>
+    /// Provides functionality to remove diacritical marks from strings.
+    /// </summary>
+    [OSInterface(Description = "Provides functionality to remove diacritical marks from strings.", IconResourceName = "OutSystems.RemoveDiacritics.resources.circumflex-accent.png")]
+    public interface IRemoveDiacritics {
+        void RemoveStringDiacritics(string text, out string cleanText);
+    }
+}
---
+++ RemoveDiacritics.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace OutSystems.RemoveDiacritics {
+
+    public class RemoveDiacritics : IRemoveDiacritics {
+        
+        /// <summary>
+		/// Removes accents from text
+		/// </summary>
+		/// <param name="ssText"></param>
+		/// <param name="ssCleanText"></param>
+		public void RemoveStringDiacritics(string text, out string cleanText)
+        {
+            cleanText = "";
+
+            var cleanString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var s in cleanString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(s);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                    stringBuilder.Append(s);
+            }
+
+            cleanText = stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+
+        } // RemoveStringDiacritics
+    }
+}
